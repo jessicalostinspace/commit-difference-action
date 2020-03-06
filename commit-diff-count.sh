f7eddd788494ebb7a99dc84eb61c101f5abbf976
Jessica Wilson <jwilson@zipwhip.com>
2020-03-05 18:44:28 -0800

    Removed verbose output

--- commit-diff-count.sh
+++ commit-diff-count.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -eoux pipefail
+set -eou pipefail
 
 baseBranch=$1
 secondaryBranch=$2
@@ -9,6 +9,7 @@ commitDiffCount=''
 baseBranchPath=$(git branch -r | grep "$baseBranch" | xargs)
 secondaryBranchPath=$(git branch -r | grep "$secondaryBranch" | xargs)
 
-commitDiffCount=$(git log --oneline $secondaryBranchPath \^$baseBranchPath | wc -l)
+commitDiffCount=$(git log --oneline 
+"$secondaryBranchPath" \^"$baseBranchPath" | wc -l)
 
 echo '{"commitDiffCount": "'"$commitDiffCount"'"}' 
