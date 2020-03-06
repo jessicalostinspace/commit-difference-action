bb4122cb45f04b73d218c20e263140c493f73fe9
Jessica Wilson <jwilson@zipwhip.com>
2020-03-05 18:18:13 -0800

    escaped circumflex

--- commit-diff-count.sh
+++ commit-diff-count.sh
@@ -3,13 +3,12 @@ set -eoux pipefail
 
 baseBranch=$1
 secondaryBranch=$2
-circumflex='^'
 
 commitDiffCount=''
 
 baseBranchPath=$(git branch -r | grep $baseBranch)
 secondaryBranchPath=$(git branch -r | grep $secondaryBranch)
 
-commitDiffCount=$(echo $(git log --oneline $secondaryBranchPath $circumflex$baseBranchPath | wc -l))
+commitDiffCount=$(echo $(git log --oneline $secondaryBranchPath \^$baseBranchPath | wc -l))
 
 echo '{"commitDiffCount": "'$commitDiffCount'"}' 
