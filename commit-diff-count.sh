dcc8bc76b38497f6d017f631f5abbb0542da6d07
Jessica Wilson <jwilson@zipwhip.com>
2020-03-05 18:01:46 -0800

    grab remote branches

--- commit-diff-count.sh
+++ commit-diff-count.sh
@@ -6,6 +6,9 @@ secondaryBranch=$2
 
 commitDiffCount=''
 
+baseBranch=$(git branch -r | grep $basebranch)
+secondaryBranch=$(git branch -r | grep $secondarybranch)
+
 commitDiffCount=$(echo $(git log --oneline $secondaryBranch ^$baseBranch | wc -l))
 
 echo '{"commitDiffCount": "'$commitDiffCount'"}' 
